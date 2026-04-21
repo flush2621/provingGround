4ab7db210a69f83531ce06cf3f4c73f132dfc20b
flush2621 <6651815@qq.com>
2026-04-21 10:28:43 +0800

    雨水问题改进

--- leetcode/test07_2points.go
+++ leetcode/test07_2points.go
@@ -5,60 +5,98 @@ import (
 )
 
 func trapx(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+	l, r := 0, len(height)-1
+	lmax, rmax := 0, 0
+	result := 0
+	for l < r {
+		if height[l] < height[r] {
+			if height[l] > lmax {
+				lmax = height[l]
+			} else {
+				result += lmax - height[l]
+			}
+			l++
+		} else {
+			if height[r] > rmax {
+				rmax = height[r]
+			} else {
+				result += rmax - height[r]
+			}
+			r--
+		}
+	}
+	return result
+}
+
+func trap(height []int) int {
 	result := 0
 	length := len(height)
-	for i := 0; i < length; {
+	i := 0
+
+	for i < length {
+		// 如果当前高度为0，直接跳过
+		if height[i] == 0 {
+			i++
+			continue
+		}
+
 		pkg := 0
-		l, r := height[i], 0
+		l := height[i]
+		r := 0
 		j := i + 1
-		//fmt.Println(l, r, i, j)
+
+		// 向右找第一个 >= l 且距离 >1 的柱子
 		for ; j < length; j++ {
-			//fmt.Println(i, j, l)
-			if height[j] >= l && l > 0 && j-i > 1 {
+			if height[j] >= l && j-i > 1 {
 				r = height[j]
-				//fmt.Println(pkg)
-				if l*(j-i-1)-pkg > 0 {
-					//fmt.Println(i, j, l*(j-i-1)-pkg)
-					result += l*(j-i-1) - pkg
+				// 计算 [i, j] 区间的水量
+				water := l*(j-i-1) - pkg
+				if water > 0 {
+					result += water
 				}
 				l = r
 				i = j
 				break
 			}
-			pkg += height[j]
-			if height[j] >= l && l > 0 && j-i == 1 {
-				l = height[j]
+
+			// 相邻且右柱 >= 左柱，不积水，直接移动 i
+			if height[j] >= l && j-i == 1 {
+				i++
 				pkg = 0
+				break
+			}
+
+			// 否则累加中间柱子高度（用于后续水量计算）
+			pkg += height[j]
+		}
+
+		// 如果没找到右边界（r == 0），说明右侧没有比 l 高的柱子
+		if r == 0 && j == length {
+			// 找右侧最大值作为新的左边界
+			tmp := 0
+			for k := i + 1; k < length; k++ {
+				if height[k] > tmp {
+					tmp = height[k]
+				}
+			}
+
+			if tmp == 0 {
+				break
+			}
+
+			// 用右侧最大值替换当前高度（模拟左边界变低）
+			height[i] = tmp
+
+			// 避免死循环：如果和下一个相等，直接跳过
+			if i+1 < length && height[i] == height[i+1] {
 				i++
 			}
 		}
-		//fmt.Println("f:", i, r)
-		if r == 0 {
-			if l > 0 {
-				tmp := 0
-				for k := i + 1; k < length; k++ {
-					if height[k] > tmp {
-						tmp = height[k]
-					}
-				}
-				//fmt.Println(tmp, i, l)
-				if tmp > 0 {
-					height[i] = tmp
-				}
-
-				if i >= length-2 || tmp == 0 {
-					break
-				}
-				if height[i] == height[i+1] {
-					i++
-				}
-			} else {
-				i++
-			}
-
-		}
-
 	}
+
 	return result
 }
 
@@ -66,8 +104,8 @@ func main() {
 	//nums := []int{0, 1, 0, 2, 1, 0, 1, 3, 1, 2, 1, 2, 1}
 	//nums := []int{4, 2, 3}
 	//nums := []int{5, 4, 1, 2}
-	//nums := []int{0, 1, 2, 0, 3, 0, 1, 2, 0, 0, 4, 2, 1, 2, 5, 0, 1, 2, 0, 2}
-	nums := []int{8, 5, 4, 1, 8, 9, 3, 0, 0}
+	nums := []int{0, 1, 2, 0, 3, 0, 1, 2, 0, 0, 4, 2, 1, 2, 5, 0, 1, 2, 0, 2}
+	//nums := []int{8, 5, 4, 1, 8, 9, 3, 0, 0}
 	fmt.Println(nums)
 	fmt.Println(trapx(nums))
 }
